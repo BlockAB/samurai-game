ba550c72688d6b50f48b38224bafc9a4ae5a956b
Abdul Basit <alatif289.al@gmail.com>
2022-05-08 00:32:06 -0700

    create player and enemy

--- app.js
+++ app.js
@@ -5,3 +5,32 @@ canvas.width = 1024;
 canvas.height = 576;
 
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+class Sprite {
+  constructor(position){
+    this.position = position 
+  }
+
+  draw(){
+
+    ctx.fillStyle = 'red'
+    ctx.fillRect(this.position.x, this.position.y, 50, 150)
+  
+  }
+}
+
+const player = new Sprite({
+  x: 0,
+  y: 0
+});
+
+player.draw()
+
+const enemy = new Sprite({
+  x: 400,
+  y: 100
+});
+
+enemy.draw()
+
+  console.log(player)
